4205b335b2b09c87a8a2bfd826d6fd774d49cae0
Rodrigo Bostelmann <50453044+rbbostelmann@users.noreply.github.com>
2022-07-11 14:30:54 -0300

    Add files via upload
    bugs fixed

---
+++ src/Conta.java
@@ -0,0 +1,47 @@
+public abstract class Conta implements IConta {
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    public Conta() {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    @Override
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    protected void imprimirInfoComuns() {
+        System.out.println(String.format("Agencia: %d", this.agencia));
+        System.out.println(String.format("Numero: %d", this.numero));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
+    }
+}
